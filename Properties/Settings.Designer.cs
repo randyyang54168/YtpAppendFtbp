068b0f8154aff56a7f36960fa4eed24b86b1aecd
randy.yang <randy.yang54168@outlook.com>
2025-04-24 22:02:55 +0800

    Rename to .exe

--- Properties/Settings.Designer.cs
+++ Properties/Settings.Designer.cs
@@ -8,7 +8,7 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-namespace YoutubePlaylistConvert.Properties {
+namespace YtpAppendFtbp.Properties {
     
     
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
